bdf81a0d1d9ad75a5a71438ea00c56679f277535
Sione Tenifa <johntenifa@yahoo.com>
2019-03-08 15:38:44 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+var flaSh = document.getElementById("flash")
+
+
+
+
+flaSh.addEventListener('click', function() {
+  document.getElementById('flash').innerHTML = "flash"
+
+  
+  
+})
+
+ 
+
+
+
+
